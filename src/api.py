3393bf3869dd93d2175f9ae0bee6b0c42e88f37b
kyystt <gui.sobreira10@gmail.com>
2025-11-24 00:48:16 -0300

    Retirei o primeiro endpoint de teste

--- src/api.py
+++ src/api.py
@@ -6,23 +6,6 @@ from src.extensions import db
 
 api_bp = Blueprint('api', __name__)
 
-@api_bp.route('/agencias')
-def get_agencias():
-    try:
-        sql = text("SELECT * FROM Agencia LIMIT 5")
-        result = db.session.execute(sql)
-
-        agencias_list = [dict(row._mapping) for row in result]
-
-        return jsonify({
-            "status": "success",
-            "message": "foi!",
-            "data": agencias_list
-        })
-
-    except Exception as e:
-        return jsonify({ "status": "error", "message":str(e) }), 500
-
 @api_bp.route('/paradas/<id_parada>')
 def get_parada(id_parada):
     sql = text("SELECT * FROM Parada WHERE id_parada = :id")
